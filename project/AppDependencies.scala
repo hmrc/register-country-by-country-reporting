a16a43ca56902eca2e059de14431936d6d662d64
Carl Thompson <carl.thompson@opencastsoftware.com>
2024-07-29 12:19:37 +0100

    missed removeable module

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,11 @@ object AppDependencies {
   private val bootstrapVersion = "9.0.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test,
     "org.mockito" %% "mockito-scala" % "1.17.37" % Test,
     "wolfendale" %% "scalacheck-gen-regexp" % "0.1.2" % Test,
