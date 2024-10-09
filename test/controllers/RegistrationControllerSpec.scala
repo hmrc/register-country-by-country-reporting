fae3f589ade0737a846d43ee7d5d214c09fceada
oarshad <omer.arshad@digital.hmrc.gov.uk>
2024-10-09 05:59:33 +0100

    DAC6-3330: Update libraries and fix code formatting
    Upgraded Play framework and Bootstrap libraries to their latest versions in build files. Added missing license header to a test specification file and corrected code formatting in controller and test files for consistency.

--- test/controllers/RegistrationControllerSpec.scala
+++ test/controllers/RegistrationControllerSpec.scala
@@ -28,7 +28,7 @@ import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.Json
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{POST, route, status, _}
+import play.api.test.Helpers.{route, status, POST, _}
 import play.api.{Application, Configuration}
 import uk.gov.hmrc.auth.core.AuthConnector
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
