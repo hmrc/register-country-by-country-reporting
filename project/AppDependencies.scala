6089d8444f9aad551dd934cb473404895d62691c
Carl Thompson <carl.thompson@opencastsoftware.com>
2026-03-04 13:28:50 +0000

    DAC6-4146 | update deps and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object AppDependencies {
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
@@ -10,7 +10,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test,
-    "org.scalamock" %% "scalamock" % "7.5.2" % Test,
+    "org.scalamock" %% "scalamock" % "7.5.5" % Test,
     "io.github.wolfendale" %% "scalacheck-gen-regexp" % "1.1.0" % Test
   )
 
